edf91a527610693474f8d15d74605ff982d1ea41
Dmytro <dima.shutko5@gmail.com>
2026-04-01 14:37:18 +0200

    added passwords for users

--- task.sql
+++ task.sql
@@ -1,7 +1,7 @@
 CREATE USER 'webappuser'@'%';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* to 'webappuser'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* to 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 
 CREATE USER 'deploymentuser'@'%';
 
-GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
